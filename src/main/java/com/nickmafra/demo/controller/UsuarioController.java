24efaf699ac23eab8f2b030cfa0953519175b293
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-05-06 10:38:34 -0300

    Add persistência mock
    - Persistência direto na controller.
    - Uso de lista ao retornar
    - Uso de objeto por parâmetro
    - Uso do método HTTP POST

--- src/main/java/com/nickmafra/demo/controller/UsuarioController.java
+++ src/main/java/com/nickmafra/demo/controller/UsuarioController.java
@@ -1,25 +1,28 @@
 package com.nickmafra.demo.controller;
 
 import com.nickmafra.demo.model.Usuario;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
 
-    @GetMapping
-    public Usuario get(@RequestParam(defaultValue = "Fulano") String nome,
-                       @RequestParam(defaultValue = "Mafra") String sobrenome) {
+    private static final Usuario USUARIO = new Usuario("Nicolas", "Mafra", LocalDate.of(1997, 7, 29));
+    private List<Usuario> usuarios = new ArrayList<>(Collections.singleton(USUARIO));
 
-        Usuario usuario = new Usuario();
-        usuario.setNome(nome);
-        usuario.setSobrenome(sobrenome);
-        usuario.setDataNascimento(LocalDate.of(1997, 7, 29));
+    @GetMapping
+    public List<Usuario> getAll() {
+        return usuarios;
+    }
+
+    @PostMapping
+    public Usuario post(@RequestBody Usuario usuario) {
+        usuarios.add(usuario);
         return usuario;
     }
 }
