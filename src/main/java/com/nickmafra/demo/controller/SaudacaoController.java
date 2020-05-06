c6537bafdd71df6a48fcf28cd056e2ccaa01b022
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-05-06 10:15:08 -0300

    Ajustes SaudacaoController

--- src/main/java/com/nickmafra/demo/controller/SaudacaoController.java
+++ src/main/java/com/nickmafra/demo/controller/SaudacaoController.java
@@ -2,14 +2,15 @@ package com.nickmafra.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/saudacao")
 public class SaudacaoController {
 
-    @GetMapping()
-    private String get(String nome) {
-        return "Olá, " + (nome == null ? "World" : nome) + "!";
+    @GetMapping
+    public String get(@RequestParam(defaultValue = "mundo") String nome) {
+        return "Olá, " + nome + "!";
     }
 }
