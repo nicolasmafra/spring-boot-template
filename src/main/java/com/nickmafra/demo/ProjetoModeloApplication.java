6f24c1151a59fc8480aef8e1d57fd2da814dcb02
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-05-06 09:25:19 -0300

    Commit inicial
    A partir do Spring Initializr

---
+++ src/main/java/com/nickmafra/demo/ProjetoModeloApplication.java
@@ -0,0 +1,13 @@
+package com.nickmafra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoModeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickmafra/demo/ProjetoModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickmafra.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
