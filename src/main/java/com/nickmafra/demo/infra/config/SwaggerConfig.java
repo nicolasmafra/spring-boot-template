b3d7c2b39181e03453ac37dfcd50fbaf8db25de6
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-12-07 10:59:22 -0300

    Ajustes no Swagger

--- src/main/java/com/nickmafra/demo/infra/config/SwaggerConfig.java
+++ src/main/java/com/nickmafra/demo/infra/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.nickmafra.demo.infra.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
@@ -12,6 +13,7 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,13 +23,19 @@ public class SwaggerConfig {
 
     private static final String AUTH_NAME = "Token";
     private static final AuthorizationScope[] AUTH_SCOPE_NONE = {};
+    private static final List<String> PATTERNS = Arrays.asList("/api/**", "/login");
 
     @Bean
     public Docket postsApi() {
-        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+        return new Docket(DocumentationType.SWAGGER_2)
                 .securityContexts(Collections.singletonList(securityContext()))
                 .securitySchemes(securitySchemes())
-                .select().paths(PathSelectors.any()).build();
+                .select().paths(this::pathMatches)
+                .build();
+    }
+
+    private boolean pathMatches(String path) {
+        return PATTERNS.stream().anyMatch(pattern -> new AntPathMatcher().match(pattern, path));
     }
 
 
