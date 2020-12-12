f8d4c8464327b48b6d8e2fcd2b7b2307db91d179
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-12-12 13:06:56 -0300

    Add String to LocalDateTime json parse

--- src/main/java/com/nickmafra/demo/infra/config/WebConfig.java
+++ src/main/java/com/nickmafra/demo/infra/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.nickmafra.demo.infra.config;
 import com.nickmafra.demo.converter.StringToDirectionConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToDirectionConverter());
+
+        // String to LocalDateTime parse
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(registry);
     }
 }
