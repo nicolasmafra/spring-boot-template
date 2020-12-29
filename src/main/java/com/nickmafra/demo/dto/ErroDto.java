7d3d943acb03bedbf699b77def7c769d22ade9ca
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-12-29 15:33:16 -0300

    Updates no AppErrorHandler
    Agora trata mensagens de baixo nível também (404, 403...)

--- src/main/java/com/nickmafra/demo/dto/ErroDto.java
+++ src/main/java/com/nickmafra/demo/dto/ErroDto.java
@@ -13,24 +13,4 @@ public class ErroDto {
     private String detalhes;
     @JsonIgnore
     private Throwable throwable;
-
-    public static ErroDto criar(Object... args) {
-        ErroDto dto = new ErroDto();
-        for (Object arg : args) {
-            if (arg instanceof Throwable) {
-                Throwable t = (Throwable) arg;
-                if (dto.getThrowable() != null) {
-                    dto.setThrowable(t);
-                }
-                arg = t.getMessage();
-            }
-            if (arg != null) {
-                if (dto.getMensagem() == null)
-                    dto.setMensagem(arg.toString());
-                else if (dto.getDetalhes() == null)
-                    dto.setDetalhes(arg.toString());
-            }
-        }
-        return dto;
-    }
 }
